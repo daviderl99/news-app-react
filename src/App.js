5862631d456fe7958a1825b99b237cdfae4b28a2
daviderl99 <daviderlich7@hot.ee>
2022-10-24 12:50:12 +0300

    Added form and styling

--- src/App.js
+++ src/App.js
@@ -1,43 +1,69 @@
 import React from 'react';
 import axios from 'axios';
+import Form from './components/Form/Form';
+import './App.css';
 
 class App extends React.Component {
 
 	state = {
-		newsData : [],
+		newsData: [],
+    showForm: false,
 	}
 
 	componentDidMount() {
-
 		let data;
 
 		axios.get('http://localhost:8000/news/')
 		.then(res => {
 			data = res.data;
 			this.setState({
-				newsData : data	
+				newsData: data	
 			});
 		})
 		.catch(err => {})
 	}
 
+  addNewsItem = (newsItem) => {
+    this.setState({
+      newsData: [...this.state.newsData, newsItem]
+    });
+    this.toggleForm();
+  }
+
+  toggleForm = () => {
+    this.setState({
+      showForm: !this.state.showForm
+    });
+  }
+
   render() {
     return(
-    <div>
+      <div>
+        {this.state.showForm === false &&
+          <button
+            type="button"
+            className="btn btn-primary m-3"
+            onClick={this.toggleForm}
+          >
+            Add news item
+          </button>
+        }
+        {this.state.showForm === true &&
+          <Form addNewsItem={this.addNewsItem} />
+        }
         {this.state.newsData.map((news, id) => (
           <div key={id}>
-            <div className="card">
+            <div className="card m-3">
               <div className="card-body">
-                <h5 className="card-title">{news.title}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{news.date}</h6>
-                <p className="card-text">{news.lead}</p>
+                <h3 className="card-title">{news.title}</h3>
+                <h5 className="card-subtitle mb-2 text-muted">{news.date}</h5>
+                <b className="card-text">{news.lead}</b>
                 <p className="card-text">{news.text}</p>
               </div>
             </div>
           </div>
-        )
-      )}
-    </div>
+        ))}
+      </div>
     );
   }
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './Form.css';
+
+export default class Form extends Component {
+
+  state = {
+    title: '',
+    date: '',
+    lead: '',
+    text: ''
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:8000/news/", {
+        title: this.state.title,
+        date: this.state.date,
+        lead: this.state.lead,
+        text: this.state.text,
+      })
+      .then((res) => {
+        this.setState({
+          title: '',
+          date: '',
+          lead: '',
+          text: ''
+        });
+      })
+      .catch((err) => {});
+    this.props.addNewsItem(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className='form m-3'>
+        <div className="form-group">
+          <input
+            value={this.state.title}
+            onChange={this.handleInput}
+            type="text" name="title"
+            className="form-control"
+            placeholder="Title" required />
+
+          <input
+            value={this.state.date}
+            onChange={this.handleInput}
+            type="date" name="date"
+            className="form-control"
+            placeholder="Date" required />
+     
+          <input
+            value={this.state.lead}
+            onChange={this.handleInput}
+            type="text" name="lead"
+            className="form-control"
+            placeholder="Lead" />
+     
+          <input
+            value={this.state.text}
+            onChange={this.handleInput}
+            type="text" name="text"
+            className="form-control"
+            placeholder="Text" required />
+        </div>
+        <button type="submit" className="btn btn-primary">Add news</button>
+      </form>
+    )
+  }
+}
